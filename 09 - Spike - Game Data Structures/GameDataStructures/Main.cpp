dfc4074e783b9c6733efbc0f2ee49a772bca5368
unknown <103017494@student.swin.edu.au>
2023-08-21 12:13:09 +1000

    Started transition from Vectors to Linked Lists. Added InventorySlot Class Added Inventory Class with the ability to add items Begun testing on adding items

--- 09 - Spike - Game Data Structures/GameDataStructures/Main.cpp
+++ 09 - Spike - Game Data Structures/GameDataStructures/Main.cpp
@@ -18,7 +18,10 @@
 *	- Try to use emplace_back() instead of push_back(). Though they're pretty much the same,
 *	  when adding elements during construction or passing implicit types into the vector 
 *	  emplace_back() is clearly favourable.
-*/
+* 
+*	
+*	NOTES ON LINKED LISTS
+*	- AHHH FUCK */
 
 class Item {
 private:
@@ -48,71 +51,133 @@ public:
 	};
 };
 
-class Inventory {
+/*	The inventory item will be a collection of inventory slots all linked together
+*	via a linked list data structure.
+* 
+*	The slot will contain a pointer to the next slot and the item address which of
+*	what has been added to the inventory. If a slot is empty then the value is initialized 
+*	to NULL; */
+class InventorySlot{
 private:
-	/*	This vector will store item pointers as we expect to do a decent bit of
-		copying and resizing so it's probably more effecient even though our 
-		object exist in a contiguous memory chunk */
-
-	std::vector<Item*> _container;
-	int _inventory_capactity = 100;
+	InventorySlot* _next_slot;
+	Item* _slot_item;
 
 public:
-	Inventory() {
-		_container.reserve(_inventory_capactity);
-		test();
+	InventorySlot(Item* item) { _slot_item = item; _next_slot = nullptr; }
+
+	InventorySlot* getNextSlot() { return _next_slot; }
+	Item* getItem() { return _slot_item; }
+	void setNextSlot(InventorySlot* next_slot) { _next_slot = next_slot; }
+	void setItem(Item* item_ptr) { _slot_item = item_ptr; }
+
+	void test(){
+		std::cout << "Item in slot: " << getItem()->getName() << std::endl;
 	}
+};
 
-	~Inventory() {
-		_container.clear();
-		std::vector<Item*>().swap(_container);
+class Inventory {
+private:
+	InventorySlot* _head;
+	int _size;
+	int _max_size = 64;
 
-		/*	As far as I can tell shrink_to_fit() can sometimes be ignored by the compiler.
-		*	After some research I found the above line of code which should deallocate the
-		*	memory after clear().
-		_container.shrink_to_fit(); */
-	}
+public:
+	Inventory() { _head = nullptr; _size = 0;}
+	~Inventory() {}
 
-	void addItem(Item* item_ptr) {
-		_container.emplace_back(item_ptr);
-	};
+	InventorySlot* getHead() { return _head; }
+	
+	void addItem(Item* item_ptr){
+		InventorySlot* new_invent_slot = new InventorySlot(item_ptr);
 
-	void printItems() {
-		for (auto iterator = _container.begin(); iterator != _container.end(); iterator++) {
-			(*iterator)->printDescription();
+		if (_head = nullptr) { _head = new_invent_slot; }
+		else { 
+			new_invent_slot->setNextSlot(_head);	// Add new value to list
+			_head = new_invent_slot;				// Change old head pointer to new value
 		}
-	}
-
-	void removeItem(Item* item_ptr) {
-		for (int i = 0; i == _container.size(); i++) {
-			if (*iterator == item_ptr) { _container.erase(iterator); }
-		}
-	}
-	/*void removeItem(Item* item_ptr) {
-		_container.erase(std::remove_if(_container.begin(), _container.end(),
-			[](const Item* element, Item* item_ptr) {return item_ptr == element; }));
-		
-		_container.end();
-	}*/
+		_size++;
+	} 
 
 	void test() {
-		if (true) {
-			std::cout << "Vector size: " << _container.size() << std::endl;
-			std::cout << "Vector capacity: " << _container.capacity() << std::endl;
-			std::cout << "Vector max size: " << _container.max_size() << std::endl;
+		InventorySlot* traversal_ptr = _head;
+
+		while (traversal_ptr != nullptr) {
+			traversal_ptr->test();
+			traversal_ptr = traversal_ptr->getNextSlot();
 		}
 	}
 };
 
+
+//class Inventory {
+//private:
+//	/*	This vector will store item pointers as we expect to do a decent bit of
+//		copying and resizing so it's probably more effecient even though our 
+//		object exist in a contiguous memory chunk */
+//
+//	std::vector<Item*> _container;
+//	int _inventory_capactity = 100;
+//
+//public:
+//	Inventory() {
+//		_container.reserve(_inventory_capactity);
+//		test();
+//	}
+//
+//	~Inventory() {
+//		_container.clear();
+//		std::vector<Item*>().swap(_container);
+//
+//		/*	As far as I can tell shrink_to_fit() can sometimes be ignored by the compiler.
+//		*	After some research I found the above line of code which should deallocate the
+//		*	memory after clear(). 
+//		_container.shrink_to_fit(); */
+//	}
+//
+//	void addItem(Item* item_ptr) {
+//		_container.emplace_back(item_ptr);
+//	};
+//
+//	void printItems() {
+//		for (auto iterator = _container.begin(); iterator != _container.end(); iterator++) {
+//			(*iterator)->printDescription();
+//		}
+//	}
+//
+//	void removeItem(Item* item_ptr) {
+//		for (int i = 0; i == _container.size(); i++) {
+//			if (*iterator == item_ptr) { _container.erase(iterator); }
+//		}
+//	}
+//	/*void removeItem(Item* item_ptr) {
+//		_container.erase(std::remove_if(_container.begin(), _container.end(),
+//			[](const Item* element, Item* item_ptr) {return item_ptr == element; }));
+//		
+//		_container.end();
+//	}*/
+//
+//	void test() {
+//		if (true) {
+//			std::cout << "Vector size: " << _container.size() << std::endl;
+//			std::cout << "Vector capacity: " << _container.capacity() << std::endl;
+//			std::cout << "Vector max size: " << _container.max_size() << std::endl;
+//		}
+//	}
+//};
+
 int main() {
 	Item testStick("Beeeeg Testing Stick", "testType", "Good for testing data structures.");
+	Item testStick2("Beeeeger Testing Stick", "testType", "Good for testing secondary data structures.");
+	Item testStick3("BEEEEGEST Testing Stick", "testType", "Good for testing tertiary data structures.");
+	Item testStick4("MONSTROUS Testing Stick", "testType", "Good for testing quartinary data structures.");
 	Inventory inventory;
 
 	inventory.addItem(&testStick);
+	inventory.addItem(&testStick2);
+	inventory.addItem(&testStick3);
+	inventory.addItem(&testStick4);
+	
 	inventory.test();
-	inventory.printItems();
-	inventory.removeItem(&testStick);
-	inventory.printItems();
-	inventory.test();
+
 	return 0;
 }
