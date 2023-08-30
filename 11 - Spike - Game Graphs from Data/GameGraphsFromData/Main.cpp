4f6564595d1a14fb3f47fc94fc1527973fdd19c2
unknown <103017494@student.swin.edu.au>
2023-08-30 22:00:26 +1000

    Implemented file reader class from Task 10 - Lab 'File Input Output' Fixed a bug where file loader would prematurely append data causing rubbish values to be pushed to the unformatted data vectors Refactored some of the FileReader code making it easier to scale Changed accessability modifiers of some FileReader functions

--- 11 - Spike - Game Graphs from Data/GameGraphsFromData/Main.cpp
+++ 11 - Spike - Game Graphs from Data/GameGraphsFromData/Main.cpp
@@ -1,5 +1,121 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
+
+/*	Task: Task 11 - Spike
+*	Title: Game Data From Graphs
+*	Author: Thomas Horsley - 103071494
+* 
+*	Currently:  Working on using a file reader to instantiate the location game object
+*			    and there-in the world object from a text file, could use JSON later.	
+* 
+*				Will probably end up making the world object more of a world "manager" 
+*				class. So given this, we're giving it the ability to read and write saves
+*				and it will have access to the player object.
+* 
+* 
+*		Formatting:
+*			To make life easier with the functions that I've already written we're going
+*			to split the txt file using a double delimiter method.
+*				* Delimit once to split the file by room
+*					This will return a vector of strings
+*				* Iterate through each vector and split their strings into another set 
+*				  of vectors.
+*				* Use this data to instantiate location objects */ 
+
+class FileReader {
+private:
+	std::string _file_name;
+	std::ifstream _reader;
+
+	bool isComment(std::string string_data) {
+		remove(string_data.begin(), string_data.end(), ' ');
+
+		if (string_data[0] == '#') { 
+			string_data.erase(string_data.begin(), string_data.end());
+			return true; 
+		}
+		return false;
+	}
+
+	std::vector<std::string> ProcessString(std::string string_data, char delimiter) {
+		std::string line = string_data;
+		std::vector<std::string> delimited_data;
+
+		// Remove Whitespace
+		remove(line.begin(), line.end(), ' ');
+		line.pop_back(); // Get rid of junk value which is added by remove??? look into that
+
+		// If the comment isn't a line, split the string and add each element to our
+		// delimited_data vector.
+		if (!isComment(line)) {
+			delimited_data = splitLine(line, delimiter);
+		}
+
+		return delimited_data;
+	}
+
+public:
+	FileReader(std::string file_name = "") {
+		_file_name = file_name;
+
+		if (_file_name == "") { std::cout << "Error: No file name supplied. "; }
+		else { _reader.open(file_name); }
+	}
+
+	~FileReader() {	if (_reader.is_open()) { _reader.close(); }}
+
+	std::vector<std::string> splitLine(std::string string_data, char delimiter) {
+		std::vector<std::string> split_strings;
+		int start_idx = 0, end_idx = 0;
+
+		for (int i = 0; i <= string_data.size(); i++) {
+			if ((char)string_data[i] == delimiter) {
+				std::string delimited_data;
+				end_idx = i;
+				delimited_data.append(string_data, start_idx, end_idx - start_idx);
+				split_strings.push_back(delimited_data);
+
+				start_idx = end_idx + 1;
+			}
+		}
+
+		return split_strings;
+	}
+
+	std::vector<std::string> GetLinesByDelimiter(char splitter) {
+		char delimiter = splitter;
+		std::string line;
+		std::vector<std::string> formatted_strings;
+
+		if (!_reader.is_open()) { _reader.open(_file_name); }
+
+		while (std::getline(_reader, line)) {
+			for (auto it : ProcessString(line, delimiter)) {
+				formatted_strings.push_back(it);
+			}
+		}
+
+		return formatted_strings;
+	}
+
+	std::vector<std::string> GetLinesByDelimiter(char splitter, std::string string_data) {
+		char delimiter = splitter;
+		std::string line = string_data;
+		std::vector<std::string> formatted_strings;
+
+		if (!_reader.is_open()) { _reader.open(_file_name); }
+
+		while (std::getline(_reader, line)) {
+			for (auto it : ProcessString(line, delimiter)) {
+				formatted_strings.push_back(it);
+			}
+		}
+
+		return formatted_strings;
+	}
+};
 
 class Location {
 private:
@@ -42,44 +158,100 @@ public:
 	}
 };
 
+/*	The world object will also have to act as a pseudo-interface between the save data
+*	and all of the world objects. It wont have the ability to read directly from files
+*	but will have the ability to take a vector of semi-formatted data from the FileReader,
+*	process this more and assign it to the neccessary locations 
+* 
+*	The rules for the text file are as follows:
+*		Use ';' to seperate locations
+*		Use ':' to seperate location data
+*		E.g. Name1:Description1:ExitAName:ExitBName:ExitCName;
+*			 Name2:Description2:ExitAName:ExitDName:ExitZName; */
+
 class World {
 private:
 	// Wont be needed once reading from a file as we can tell when to stop
 	// when there's nothing else to read.
+	std::string _save_name = "test.txt";
+	FileReader* _reader = new FileReader(_save_name);
+	
 	Location* _current_location = nullptr;
 	std::vector<Location*> _locations;
 
+	// Any new locations are instantiated here
+	std::vector<Location*> ConstructLocations() {
+		std::vector<Location*> locations;
+
+		//Allocate Memory for rooms
+		Location* lounge_room = new Location();
+		Location* courtyard = new Location();
+		Location* bedroom = new Location();
+		Location* backyard = new Location();
+
+		//Push those pointers to _locations
+		locations.push_back(lounge_room);
+		locations.push_back(courtyard);
+		locations.push_back(bedroom);
+		locations.push_back(backyard);
+
+		return locations;
+	}
+
 public:
-	World(Location* initial_location = nullptr) {
-		_current_location = initial_location;
-		
+	/*	1.	Initialize memory for each location and push the pointers to these
+	*		locations to the locations vector
+	*	2.	Let the file reader run through the save file data and return a vector
+	*		of strings which of each contains the data for a location
+	*			* Name
+	*			* Description
+	*			* Name of Exit locations seperated with : 
+	*				To make sure this is consistent, the room must be named the same
+	*				in the text file as it is in the code. */
+
+	World() {
+		_locations = ConstructLocations();
+
 		if (_current_location != nullptr) {
 			_locations.push_back(_current_location);
 		}
 		else { std::cout << "Error: No initial location given to World constructor" << std::endl; }
 	}
 
-	void AddLocation(Location* new_location) {
-		_locations.push_back(new_location);
-	}
+	void AddLocation(Location* new_location) { _locations.push_back(new_location); }
 
 	void ChangeCurrentLocation(std::string location_name) {		
 		for (auto it : _locations) {
-			if (it->GetName() == location_name) {
-				_current_location = it;
-			}
+			if (it->GetName() == location_name) { _current_location = it; }
 		}
 	}
 
 	// Wrapper for the current locations methods
-	void ShowCurrentLocation() {
-		_current_location->ShowDetails();
-	}
+	void ShowCurrentLocation() { _current_location->ShowDetails(); }
+	void ShowLocations() { for (auto it : _locations) { it->ShowDetails(); }}
 
-	void ShowLocations() {
-		for (auto it : _locations) {
-			it->ShowDetails();
+	// For the assumptions made see
+	void LoadLocationData() {
+		//Delimiters
+		char semicolon = ';';
+		char colon = ':';
+
+		std::vector<std::string> unformatted_room_data = _reader->GetLinesByDelimiter(semicolon);
+		std::vector<std::vector<std::string>> formatted_room_data;
+
+		for (auto it : unformatted_room_data) { 
+			it += colon; // Add the delimiter to the end of the line
+			std::vector<std::string> formatted_line = _reader->splitLine(it, colon);
+			formatted_room_data.push_back(formatted_line);
 		}
+
+		for (int i = 0; i < formatted_room_data.size(); i++) {
+			for (int j = 0; j < formatted_room_data[i].size(); j++) {
+				std::cout << formatted_room_data[i][j] << std::endl;
+			}
+		}
+
+		std::cout << "testline" << std::endl;
 	}
 };
 
@@ -91,24 +263,8 @@ int main() {
 	* 
 	* Testing: once the character goes into L3 they can only go to S */ 
 
-	//Forward declate the locations so they can reference each other
-	Location init_location, loc1, loc2, loc3;
 	//Initial setup
-	init_location.SetAll("Start room", "Cold, dank and rank", { &loc1, &loc3 });
-	World world(&init_location);
+	World world;
 
-	//Define & add rooms
-	loc1.SetAll("Location 1", "A bit sunnier", { &init_location,&loc2 });
-	loc2.SetAll("Location 2", "Looks like Sullies room", { &loc1, &loc3 });
-	loc3.SetAll("Location 3", "Hey, i can get back to the start from here!", { &init_location });
-	
-
-	world.AddLocation(&loc1);
-	world.AddLocation(&loc2);
-	world.AddLocation(&loc3);
-
-	world.ShowCurrentLocation();
-
-	world.ChangeCurrentLocation("Location 1");
-	world.ShowCurrentLocation();
+	world.LoadLocationData();
 }
